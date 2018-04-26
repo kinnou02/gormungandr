863b0edfdedba7b44901ad3ddb2fd4c108db3af3
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2018-04-26 17:20:16 +0200

    authentication cache tests also mini-rename

--- auth/authentication_test.go
+++ auth/authentication_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/CanalTP/gormungandr"
+	cache "github.com/patrickmn/go-cache"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
@@ -28,20 +29,20 @@ func expectAuthError(mock sqlmock.Sqlmock) sqlmock.Sqlmock {
 	return mock
 }
 
-func expectIsAuthorizeSuccess(mock sqlmock.Sqlmock) sqlmock.Sqlmock {
+func expectIsAuthorizedSuccess(mock sqlmock.Sqlmock) sqlmock.Sqlmock {
 	rows := sqlmock.NewRows([]string{"bool"}).
 		AddRow(true)
 	mock.ExpectQuery("SELECT true").WillReturnRows(rows)
 	return mock
 }
 
-func expectIsAuthorizeNoResult(mock sqlmock.Sqlmock) sqlmock.Sqlmock {
+func expectIsAuthorizedNoResult(mock sqlmock.Sqlmock) sqlmock.Sqlmock {
 	rows := sqlmock.NewRows([]string{"bool"})
 	mock.ExpectQuery("SELECT true").WillReturnRows(rows)
 	return mock
 }
 
-func expectIsAuthorizeError(mock sqlmock.Sqlmock) sqlmock.Sqlmock {
+func expectIsAuthorizedError(mock sqlmock.Sqlmock) sqlmock.Sqlmock {
 	mock.ExpectQuery("SELECT true").WillReturnError(sql.ErrTxDone)
 	return mock
 }
@@ -96,7 +97,7 @@ func TestIsAuthorized(t *testing.T) {
 	t.Parallel()
 	db, mock := newMock()
 	defer db.Close()
-	mock = expectIsAuthorizeSuccess(mock)
+	mock = expectIsAuthorizedSuccess(mock)
 
 	user := gormungandr.User{
 		Id:   42,
@@ -129,7 +130,7 @@ func TestIsAuthorizedFailed(t *testing.T) {
 	t.Parallel()
 	db, mock := newMock()
 	defer db.Close()
-	mock = expectIsAuthorizeNoResult(mock)
+	mock = expectIsAuthorizedNoResult(mock)
 
 	user := gormungandr.User{
 		Id:   42,
@@ -146,7 +147,7 @@ func TestIsAuthorizedError(t *testing.T) {
 	t.Parallel()
 	db, mock := newMock()
 	defer db.Close()
-	mock = expectIsAuthorizeError(mock)
+	mock = expectIsAuthorizedError(mock)
 
 	user := gormungandr.User{
 		Id:   42,
@@ -155,5 +156,200 @@ func TestIsAuthorizedError(t *testing.T) {
 
 	_, err := isAuthorized(user, "fr-idf", db)
 	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestCachedAuthenticateSuccess(t *testing.T) {
+	t.Parallel()
+	db, mock := newMock()
+	defer db.Close()
+	mock = expectAuthSuccess(mock)
+	authCache := cache.New(300*time.Second, 600*time.Second)
+
+	user, err := CachedAuthenticate("mytoken", time.Now(), db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, "mylogin", user.Username) // not testing all here
+	user, err = CachedAuthenticate("mytoken", time.Now(), db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, "mylogin", user.Username)
+	assert.Equal(t, 42, user.Id)
+	assert.Equal(t, "myapp", user.AppName)
+	assert.Equal(t, "with_free_instances", user.Type)
+	assert.Equal(t, 1, user.EndPointId)
+	assert.Equal(t, "navio", user.EndPointName)
+	assert.Equal(t, "key", user.Token)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestCachedAuthenticateFail(t *testing.T) {
+	t.Parallel()
+	db, mock := newMock()
+	defer db.Close()
+	mock = expectAuthNoResult(mock)
+	authCache := cache.New(300*time.Second, 600*time.Second)
+
+	_, err := CachedAuthenticate("mytoken", time.Now(), db, authCache)
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrAuthenticationFailed, err)
+	_, err = CachedAuthenticate("mytoken", time.Now(), db, authCache)
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrAuthenticationFailed, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestCachedAuthenticateFailOutdatedThenSuccess(t *testing.T) {
+	t.Parallel()
+	db, mock := newMock()
+	defer db.Close()
+	mock = expectAuthNoResult(mock)
+	mock = expectAuthSuccess(mock)
+	authCache := cache.New(1*time.Microsecond, 1*time.Microsecond)
+
+	_, err := CachedAuthenticate("mytoken", time.Now(), db, authCache)
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrAuthenticationFailed, err)
+	time.Sleep(2 * time.Microsecond)
+	user, err := CachedAuthenticate("mytoken", time.Now(), db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, "mylogin", user.Username)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestCachedAuthenticateSuccessNocache(t *testing.T) {
+	t.Parallel()
+	db, mock := newMock()
+	defer db.Close()
+	mock = expectAuthSuccess(mock)
+	mock = expectAuthSuccess(mock)
+
+	user, err := CachedAuthenticate("mytoken", time.Now(), db, nil)
+	assert.Nil(t, err)
+	assert.Equal(t, "mylogin", user.Username)
+	user, err = CachedAuthenticate("mytoken", time.Now(), db, nil)
+	assert.Nil(t, err)
+	assert.Equal(t, "mylogin", user.Username)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestCachedAuthenticateErrorThenSuccess(t *testing.T) {
+	t.Parallel()
+	db, mock := newMock()
+	defer db.Close()
+	mock = expectAuthError(mock)
+	mock = expectAuthSuccess(mock)
+	authCache := cache.New(300*time.Second, 600*time.Second)
+
+	_, err := CachedAuthenticate("mytoken", time.Now(), db, authCache)
+	assert.NotNil(t, err)
+	user, err := CachedAuthenticate("mytoken", time.Now(), db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, "mylogin", user.Username)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestCachedIsAuthorizedSuccess(t *testing.T) {
+	t.Parallel()
+	db, mock := newMock()
+	defer db.Close()
+	mock = expectIsAuthorizedSuccess(mock)
+	authCache := cache.New(300*time.Second, 600*time.Second)
+
+	user := gormungandr.User{
+		Id:   42,
+		Type: "with_free_instances",
+	}
+
+	result, err := CachedIsAuthorized(user, "fr-idf", db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, true, result)
+	result, err = CachedIsAuthorized(user, "fr-idf", db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, true, result)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestCachedIsAuthorizedFailed(t *testing.T) {
+	t.Parallel()
+	db, mock := newMock()
+	defer db.Close()
+	mock = expectIsAuthorizedNoResult(mock)
+	authCache := cache.New(300*time.Second, 600*time.Second)
+
+	user := gormungandr.User{
+		Id:   42,
+		Type: "with_free_instances",
+	}
+
+	result, err := CachedIsAuthorized(user, "fr-idf", db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, false, result)
+	result, err = CachedIsAuthorized(user, "fr-idf", db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, false, result)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestCachedIsAuthorizedFailOutdatedThenSuccess(t *testing.T) {
+	t.Parallel()
+	db, mock := newMock()
+	defer db.Close()
+	mock = expectIsAuthorizedNoResult(mock)
+	mock = expectIsAuthorizedSuccess(mock)
+	authCache := cache.New(1*time.Microsecond, 1*time.Microsecond)
+
+	user := gormungandr.User{
+		Id:   42,
+		Type: "with_free_instances",
+	}
+
+	result, err := CachedIsAuthorized(user, "fr-idf", db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, false, result)
+	time.Sleep(2 * time.Microsecond)
+	result, err = CachedIsAuthorized(user, "fr-idf", db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, true, result)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestCachedIsAuthorizedSuccessNoCache(t *testing.T) {
+	t.Parallel()
+	db, mock := newMock()
+	defer db.Close()
+	mock = expectIsAuthorizedSuccess(mock)
+	mock = expectIsAuthorizedSuccess(mock)
+
+	user := gormungandr.User{
+		Id:   42,
+		Type: "with_free_instances",
+	}
+
+	result, err := CachedIsAuthorized(user, "fr-idf", db, nil)
+	assert.Nil(t, err)
+	assert.Equal(t, true, result)
+	result, err = CachedIsAuthorized(user, "fr-idf", db, nil)
+	assert.Nil(t, err)
+	assert.Equal(t, true, result)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestCachedIsAuthorizedErrorThenSuccess(t *testing.T) {
+	t.Parallel()
+	db, mock := newMock()
+	defer db.Close()
+	mock = expectIsAuthorizedError(mock)
+	mock = expectIsAuthorizedSuccess(mock)
+	authCache := cache.New(300*time.Second, 600*time.Second)
+
+	user := gormungandr.User{
+		Id:   42,
+		Type: "with_free_instances",
+	}
+
+	_, err := CachedIsAuthorized(user, "fr-idf", db, authCache)
+	assert.NotNil(t, err)
+	result, err := CachedIsAuthorized(user, "fr-idf", db, authCache)
+	assert.Nil(t, err)
+	assert.Equal(t, true, result)
 	assert.Nil(t, mock.ExpectationsWereMet())
 }
